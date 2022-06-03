2fd054c5596c07630fe34cf5942dce74fe24c623
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-03 02:08:09 +0200

    Moved project to src directory

--- src/rsacracker/test.py
+++ src/rsacracker/test.py
@@ -1,18 +1,18 @@
-from insecure import insecurePrimePair
-from crack import crackRSA
+from rsacracker import insecure_prime_pair
+from crack import crack_rsa
 
 def crack_difference(N, diff):
-    p, q = insecurePrimePair(N, diff)
+    p, q = insecure_prime_pair(N, diff)
     print('Generated')
 
     n = p*q
     e = 65537
     d = pow(e, -1, (p-1) * (q-1))
 
-    res = crackRSA(n, e, 1000000)
-    if (res == None):
+    res = crack_rsa(n, e, 1000000)
+    if res == None:
         return 'unsuccessful', res
-    elif (res[0] == d):
+    elif res[0] == d:
         return 'correct', res
     else:
         return 'incorrect', res
