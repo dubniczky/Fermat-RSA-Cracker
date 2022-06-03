12d0383c04df2149541b01cd748f0dc3f5508d9f
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-03 11:36:26 +0200

    Added crack result object to return from crack rsa

--- src/rsacracker/crack.py
+++ src/rsacracker/crack.py
@@ -1,11 +1,35 @@
 from math import isqrt
 
-def crack_rsa(n, e, limit):
-    a = int(isqrt(n) + 1)
+class CrackResult():
+    d: int
+    p: int
+    q: int
+    phi: int
+    steps: int
+
+    def __init__(self, d, p, q, phi, steps) -> None:
+        self.d = d
+        self.p = p
+        self.q = q
+        self.phi = phi
+        self.steps = steps
+
+
+def crack_rsa(n: int, e: int, *, limit: int = 2**16, offset: int = 0) -> CrackResult:
+    if n < 1:
+        return None
+    if e < 2:
+        return None
+    if limit < 1:
+        return None
+    if offset < 0:
+        return None
+
+    a = int( isqrt(n) + 1 )
     b = -1
     steps = 0
 
-    for i in range(limit):
+    for i in range(offset, limit):
         b2 = int(a**2 - n)
         if b2 == isqrt(b2) ** 2:
             b = int(isqrt(b2))
@@ -21,4 +45,4 @@ def crack_rsa(n, e, limit):
     phi = (p-1) * (q-1)
     #print(p, q, phi, a, b)
     d = pow(e, -1, phi)
-    return (d, p, q, phi, steps)
+    return CrackResult(d, p, q, phi, steps)
